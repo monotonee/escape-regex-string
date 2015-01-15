5c25044707f1563f15364c41ea18d9522ec3d64d
monotonee <monotonee@users.noreply.github.com>
2015-01-15 16:29:12 -0500

    Very minor change. Slightly edited comment wording.

--- test/escape-regex-string.js
+++ test/escape-regex-string.js
@@ -25,26 +25,46 @@ describe('escape-regex-string', function() {
   });
   
   it('should not accept invalid arguments', function() {
+    /**
+     * Define locally-scoped function to catch TypeError exceptions in the following tests.
+     * @param exceptionObject Object constructed by Error or a descendant.
+     * @return undefined
+     */
+    function rethrowExceptionIfNotTypeError(exceptionObject) {
+      // Depending on short-circuit evaluation in condition.
+      if (Object.prototype.toString.call(exceptionObject) !== '[object Error]'
+        || exceptionObject instanceof TypeError === false
+      ) {
+        throw exceptionObject;
+      }
+    }
+    
+    // Attempt to pass invalid first argument.
     try {
       dependencies.escapeRegexString(42);
       throw new Error('Invalid first (string) argument passed but no exception thrown.');
     }
-    catch (exceptionObject) {}
+    catch (exceptionObject) {
+      rethrowExceptionIfNotTypeError(exceptionObject);
+    }
+    // Attempt to pass invalid second argument.
     try {
       dependencies.escapeRegexString('dik-dik', 42);
       throw new Error('Invalid second (RegExp) argument passed but no exception thrown.');
     }
-    catch (exceptionObject) {}
+    catch (exceptionObject) {
+      rethrowExceptionIfNotTypeError(exceptionObject);
+    }
   });
   
-  describe('#defaultEscapeCharsRegex', function() {
+  describe('.defaultEscapeCharsRegex', function() {
     
     it('should be a non-configurable property', function() {
-      try {
-        dependencies.escapeRegexString.defaultEscapeCharsRegex = 3.14;
-        throw new Error('Property value was changed during runtime.');
-      }
-      catch (exceptionObject) {}
+      dependencies.escapeRegexString.defaultEscapeCharsRegex = 3.14;
+      dependencies.assert.notStrictEqual(
+        dependencies.escapeRegexString.defaultEscapeCharsRegex,
+        3.14
+      );
     });
     
     it('should contain a readable RegExp object', function() {
