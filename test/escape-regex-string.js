4c7538343518439c09a3e7c44737e8d33d0f78d1
monotonee <monotonee@users.noreply.github.com>
2015-01-15 10:32:50 -0500

    FIxed syntax and logic errors in test file.

--- test/escape-regex-string.js
+++ test/escape-regex-string.js
@@ -5,51 +5,51 @@ var dependencies = {
 
 describe('escape-regex-string', function() {
   
-  it('should escape regular expression tokens within a string literal'), function() {
+  it('should escape regular expression tokens within a string literal', function() {
     dependencies.assert.strictEqual(
-      escapeRegexString('- | \\ { } ( ) [ ] ^ $ + * ? .'), 
-      '\\- \\| \\\ \\{ \\} \\( \\) \\[ \\] \\^ \\$ \\+ \\* \\? \\.'
+      dependencies.escapeRegexString('- | \\ { } ( ) [ ] ^ $ + * ? .'), 
+      '\\- \\| \\\\ \\{ \\} \\( \\) \\[ \\] \\^ \\$ \\+ \\* \\? \\.'
     );
     dependencies.assert.strictEqual(
-      escapeRegexString('(Phobetron pithecium)'), 
+      dependencies.escapeRegexString('(Phobetron pithecium)'), 
       '\\(Phobetron pithecium\\)'
     );
   });
   
-  it('should use optional RegExp argument when passed instead of default'), function() {
+  it('should use optional RegExp argument when passed instead of default', function() {
     // Remove the hyphen character from the regex pattern.
     dependencies.assert.strictEqual(
-      escapeRegexString('- | \\ { } ( ) [ ] ^ $ + * ? .', /[|\\{}()[\]^$+*?.]/g), 
-      '- \\| \\\ \\{ \\} \\( \\) \\[ \\] \\^ \\$ \\+ \\* \\? \\.'
+      dependencies.escapeRegexString('- | \\ { } ( ) [ ] ^ $ + * ? .', /[|\\{}()[\]^$+*?.]/g), 
+      '- \\| \\\\ \\{ \\} \\( \\) \\[ \\] \\^ \\$ \\+ \\* \\? \\.'
     );
   });
   
-  it('should not accept invalid arguments'), function() {
+  it('should not accept invalid arguments', function() {
     try {
-      escapeRegexString(42);
+      dependencies.escapeRegexString(42);
       throw new Error('Invalid first (string) argument passed but no exception thrown.');
     }
     catch (exceptionObject) {}
     try {
-      escapeRegexString('dik-dik', 42);
+      dependencies.escapeRegexString('dik-dik', 42);
       throw new Error('Invalid second (RegExp) argument passed but no exception thrown.');
     }
     catch (exceptionObject) {}
   });
   
-  describe('#defaultEscapeCharsRegex' function() {
+  describe('#defaultEscapeCharsRegex', function() {
     
-    it('should be a non-configurable property'), function() {
+    it('should be a non-configurable property', function() {
       try {
-        escapeRegexString.defaultEscapeCharsRegex = 3.14;
+        dependencies.escapeRegexString.defaultEscapeCharsRegex = 3.14;
         throw new Error('Property value was changed during runtime.');
       }
       catch (exceptionObject) {}
     });
     
-    it('should contain a readable RegExp object'), function() {
+    it('should contain a readable RegExp object', function() {
       dependencies.assert.strictEqual(
-        Object.prototype.toString.call(escapeRegexString.defaultEscapeCharsRegex),
+        Object.prototype.toString.call(dependencies.escapeRegexString.defaultEscapeCharsRegex),
         '[object RegExp]'
       );
     });
